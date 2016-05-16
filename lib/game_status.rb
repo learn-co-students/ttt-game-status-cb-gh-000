e0adc2e8069278752f5901143c827ddc3da298b4
Avidor Turkewitz <aviturkewitz@gmail.com>
2016-05-16 09:58:38 -0400

    Update position_take? method to use index

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -1,6 +1,6 @@
 # Helper Method
-def position_taken?(board, location)
-  !(board[location].nil? || board[location] == " ")
+def position_taken?(board, index)
+  !(board[index].nil? || board[index] == " ")
 end
 
 # Define your WIN_COMBINATIONS constant
