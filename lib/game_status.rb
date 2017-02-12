ebc9699a21b68d36e9aaea0180b2a0f8e1d045a9
Alpay Can ŞENER <alpaycansener@hotmail.com>
2017-02-12 07:49:36 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,54 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+
+WIN_COMBINATIONS = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [6,4,2]]
+
+def won?(board)
+  WIN_COMBINATIONS.each do |win_combination|
+    win_index_1 = win_combination[0]
+    win_index_2 = win_combination[1]
+    win_index_3 = win_combination[2]
+
+    position_1 = board[win_index_1]
+    position_2 = board[win_index_2]
+    position_3 = board[win_index_3]
+
+    if (position_1 == "X" && position_2 == "X" && position_3 == "X") || (position_1 == "O" && position_2 == "O" && position_3 == "O")
+      return win_combination
+    else
+      false
+    end
+  end
+  return false
+end
+
+def full?(board)
+  return !(board.any? { |index| index == " " })
+end
+
+def draw?(board)
+  if won?(board) == false && full?(board) == true
+    return true
+  elsif won?(board) == false && full?(board) == false
+    return false
+  else
+    return false
+  end
+end
+
+def over?(board)
+  return won?(board) || full?(board) || draw?(board)
+end
+
+def winner(board)
+  win_combination = won?(board)
+  if win_combination != false
+    if win_combination[1] == "X"
+      return "X"
+    else
+      return "O"
+    end
+  end
+  return nil
+end
