7ec398445f1f2e5bbb77712013cc121efd453a43
Alishaan Asaria <alishaan.asaria@gmail.com>
2017-03-05 14:08:27 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,63 +4,59 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+
 WIN_COMBINATIONS = [
-  [0, 1, 2],
-  [3, 4, 5],
-  [6, 7, 8],
-  [0, 3, 6],
-  [1, 4, 7],
-  [2, 5, 8],
-  [0, 4, 8],
-  [2, 4, 6]
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [2,4,6]
 ]
 
 def won?(board)
-  winner = false
+  victory = false
   WIN_COMBINATIONS.each do |combination|
-    win_pos_1 = combination[0]
-    win_pos_2 = combination[1]
-    win_pos_3 = combination[2]
-
-    pos_1 = board[win_pos_1]
-    pos_2 = board[win_pos_2]
-    pos_3 = board[win_pos_3]
-
-    if position_taken?(board, win_pos_1) && position_taken?(board, win_pos_2) &&
-       position_taken?(board, win_pos_3)
-
-       pos_1 = board[win_pos_1]
-       pos_2 = board[win_pos_2]
-       pos_3 = board[win_pos_3]
-
-       if pos_1 == pos_2 && pos_2 == pos_3
-         winner = combination
-         break
-       end
+    if board[combination[0]] == "X" && board[combination[1]] == "X" && board[combination[2]] == "X"
+      victory = combination
+      break
+    elsif board[combination[0]] == "O" && board[combination[1]] == "O" && board[combination[2]] == "O"
+      victory = combination
+      break
     end
   end
-  return winner
+  return victory
 end
 
 def full?(board)
-  is_full = board.all? do |square|
-    square == "X" || square == "O"
+  is_full = board.all? do |block|
+    block == "X" || block == "O"
   end
 end
 
 def draw?(board)
-  !won?(board) && full?(board)
+  if full?(board) && !won?(board)
+    return true
+  else
+    return false
+  end
 end
 
 def over?(board)
-  won?(board) || full?(board) || draw?(board)
+  if full?(board) || won?(board) || draw?(board)
+    return true
+  else
+    return false
+  end
 end
 
 def winner(board)
-  win = won?(board)
-  if win != false
-    board[win[0]]
-  else
-    return nil
-  end
+  winner = won?(board)
+    if winner == false
+      return nil
+    else
+      board[winner[0]]
+    end
 end
