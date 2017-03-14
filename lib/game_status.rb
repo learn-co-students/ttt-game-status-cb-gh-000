8cc2c543d70088d73c2be75181325f3eb5ef3fd5
Wending Peng <wendingp@gmail.com>
2017-03-14 03:04:43 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,56 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+]
+
+def won?(board)
+    WIN_COMBINATIONS.each do |win_combination|
+        win_index_1 = win_combination[0]
+        win_index_2 = win_combination[1]
+        win_index_3 = win_combination[2]
+
+        position_1 = board[win_index_1]
+        position_2 = board[win_index_2]
+        position_3 = board[win_index_3]
+
+        if position_1 == "X" && position_2 == "X" &&
+            position_3 == "X"
+            return win_combination
+        elsif position_1 == "O" && position_2 == "O" &&
+            position_3 == "O"
+            return win_combination
+        end
+    end
+    return nil
+end
+
+def full?(board)
+    return board.all? {|item| item == "X" || item == "O"}
+end
+
+def draw?(board)
+    return full?(board) && !won?(board)
+end
+
+def over?(board)
+    return full?(board) || won?(board) || draw?(board)
+end
+
+def winner(board)
+    if !won?(board)
+        return nil
+    end
+    return board[won?(board)[0]]
+end
+
+board = ["X", " ", " ", " ", "X", " ", " ", " ", "X"]
+puts(won?(board))
