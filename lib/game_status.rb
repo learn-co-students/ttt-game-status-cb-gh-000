b8bd719851aff0903ca7e73aa11cc52470cd8abf
Lashaunna Samuels <shaunna191@hotmail.com>
2017-07-18 20:37:35 -0400

    Done

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -1,6 +1,46 @@
-# Helper Method
-def position_taken?(board, index)
-  !(board[index].nil? || board[index] == " ")
-end
+require 'pry'
 
 # Define your WIN_COMBINATIONS constant
+
+WIN_COMBINATIONS = [
+  [0, 1, 2], # Top row
+  [3, 4, 5], # Middle row
+  [6, 7, 8], # Bottom row
+  [0, 3, 6], # Left column
+  [1, 4, 7], # Middle column
+  [2, 5, 8], # Right column
+  [0, 4, 8], # Left horizontal
+  [2, 4, 6]  # Right horizontal
+].freeze
+
+# Helper Method
+def position_taken?(board, index)
+  !(board[index].nil? || board[index] == ' ')
+end
+
+# Has a player won?
+def won?(board)
+  WIN_COMBINATIONS.detect do |combo|
+    board[combo[0]] == board[combo[1]] &&
+    board[combo[1]] == board[combo[2]] &&
+    position_taken?(board, combo[0])
+  end
+end
+
+def full?(board)
+  board.all? { |x| x == 'X' || x == 'O'}
+end
+
+def draw?(board)
+  full?(board) && !won?(board)
+end
+
+def over?(board)
+  won?(board) || full?(board) || draw?(board)
+end
+
+def winner(board)
+  if win_char = won?(board)
+    board[win_char.first]
+    end
+end
