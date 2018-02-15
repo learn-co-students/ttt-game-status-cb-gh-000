adc0b56273e6a6b95c46c20e91af5a56df96178c
Kaspar JohannesPärtel <kasparjohannes.partel@tptlive.ee>
2018-02-15 17:14:23 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,53 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+
+WIN_COMBINATIONS = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,4,8],
+  [2,4,6],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8]
+]
+
+def won?(board)
+  WIN_COMBINATIONS.find do |i|
+    board[i[0]] == board[i[1]] && board[i[0]] == board[i[2]] && position_taken?(board, i[1])
+  end
+end
+
+def full?(board)
+  if board.any? { |e| e == '' || e == ' ' || e == nil }
+    return false
+  else
+    return true
+  end
+end
+
+def draw?(board)
+  if !won?(board) && full?(board)
+    return true
+  else
+    return false
+  end
+end
+
+def over?(board)
+  if won?(board) || full?(board)
+    return true
+  else
+    return false
+  end
+end
+
+def winner(board)
+  if won?(board)
+    won_token = won?(board)
+    return board[won_token[0]]
+  else
+    return nil
+  end
+end
