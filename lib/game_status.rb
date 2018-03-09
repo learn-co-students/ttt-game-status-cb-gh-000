cd50167cbb9591c6eddb2aba83932657fd364c6e
Omar Nava <omarnavaalvarez@gmail.com>
2018-03-09 00:29:21 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,48 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [2,4,6]
+]
+
+def won?(board)
+  WIN_COMBINATIONS.detect do |win_combo|
+    if (board[win_combo[0]]) == "X" && (board[win_combo[1]]) == "X" && (board[win_combo[2]]) == "X"
+      return win_combo
+    elsif (board[win_combo[0]]) == "O" && (board[win_combo[1]]) == "O" && (board[win_combo[2]]) == "O"
+      return win_combo
+    end
+      false
+  end
+end
+
+def full?(board)
+  board.all?{|place| place != " "}
+end
+
+def draw?(board)
+  !(won?(board)) && (full?(board))
+end
+
+def over?(board)
+  (won?(board)) || (full?(board)) || (draw?(board))
+end
+
+def winner(board)
+  WIN_COMBINATIONS.detect do |win_combo|
+    if (board[win_combo[0]]) == "X" && (board[win_combo[1]]) == "X" && (board[win_combo[2]]) == "X"
+      return "X"
+    elsif (board[win_combo[0]]) == "O" && (board[win_combo[1]]) == "O" && (board[win_combo[2]]) == "O"
+      return "O"
+    else
+      nil
+    end
+  end
+end
