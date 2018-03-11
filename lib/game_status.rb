a0fe39a66fe27a49192a48d2e23037c5c38cb572
Shlok Joshi <shlokjoshi@gmail.com>
2018-03-11 12:09:21 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,30 +4,41 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
-WIN_COMBINATIONS = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]]
+WIN_COMBINATIONS = [
+
+[0,1,2],
+[3,4,5],
+[6,7,8],
+[0,3,6],
+[1,4,7],
+[2,5,8],
+[0,4,8],
+[2,4,6]
+
+]
 
 def won?(board)
   WIN_COMBINATIONS.each do |win_combination|
     win_index_1 = win_combination[0]
     win_index_2 = win_combination[1]
-    win_index_3 = win_combination[3]
+    win_index_3 = win_combination[2]
 
-  position_1 = board[win_index_1]
-  position_2 = board[win_index_2]
-  position_3 = board[win_index_3]
+  pos1 = board[win_index_1]
+  pos2 = board[win_index_2]
+  pos3 = board[win_index_3]
 
-  if position_1 == "X" && position_2 == "X" && position_3 == "X"
+  if pos1 == "X" && pos2 == "X" && pos3 == "X"
     return win_combination
-  elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
+  elsif pos1 == "O" && pos2 == "O" && pos3 == "O"
     return win_combination
+    end
   end
-end
 return false
 end
 
 def full?(board)
   board.each do |token|
-    if token != "X" || token != "Y"
+    if token != "X" && token != "O"
       return false
     end
   end
