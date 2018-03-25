ffe6f76ceca3a4a1d052aae6c74dcdcdd5ca012d
Patricia Nykaza <pnykaza@calstatela.edu>
2018-03-25 19:32:29 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,48 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+
+WIN_COMBINATIONS = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [2,4,6],
+  [0,4,8]
+  ]
+  
+
+def won?(board)
+  
+  WIN_COMBINATIONS.each do |win_combo|
+   
+    win_index_1 = win_combo[0]
+    win_index_2 = win_combo[1]
+    win_index_3 = win_combo[2]
+     
+    position_1 = board[win_index_1]
+    position_2 = board[win_index_2]
+    position_3 = board[win_index_3]
+ 
+ end
+   if position_1 == "X" && position_2 == "X" && position_3 == "X"
+    return win_combo 
+  else
+    false
+  end
+end
+
+   
+  
+  
+  
+
+
+
+
+
+
+
+
