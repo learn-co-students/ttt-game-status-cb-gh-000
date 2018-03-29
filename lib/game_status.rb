d6bb0edc279cd0e945ed58c74b113e525fe7b3ff
Patricia Nykaza <pnykaza@calstatela.edu>
2018-03-29 20:53:52 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -18,6 +18,7 @@ WIN_COMBINATIONS = [
   
 
 def won?(board)
+ 
   WIN_COMBINATIONS.each do |win_combo|
       win_index_1 = win_combo[0]
       win_index_2 = win_combo[1]
@@ -31,17 +32,56 @@ def won?(board)
         return win_combo
       elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
         return win_combo
-      else
-        return false
       end
+    end
+      return false
+end
+
+
+def full?(board)
+    board.all? {|square| square == "X" || square == "O" }
+end
+  
+  
+def draw?(board)
+  if full?(board) && won?(board) == false
+    return true
+  elsif full?(board) == false && won?(board) == false
+    return false
+  elsif won?(board)
+    return false
+  end 
+end
+  
+
+def over?(board)
+  if won?(board) || draw?(board) || full?(board)
+    return true
+  else
+    return false
   end
 end
 
+def winner(board)
+   WIN_COMBINATIONS.each do |win_combo|
+      win_index_1 = win_combo[0]
+      win_index_2 = win_combo[1]
+      win_index_3 = win_combo[2]
+       
+      position_1 = board[win_index_1]
+      position_2 = board[win_index_2]
+      position_3 = board[win_index_3]
+      
+      if position_1 == "X" && position_2 == "X" && position_3 == "X"
+        return "X"
+      elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
+        return "O"
+      end
+    end
+      return nil
    
+end
   
-  
-  
-
 
 
 
