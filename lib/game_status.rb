f04fab46eb1f811eb1054d3451bb6242f0a12bf1
Luis Rodriguez <zacha@tutanota.com>
2018-04-01 16:18:28 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,60 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+  [0,1,2],  # Top row
+  [3,4,5],  # Middle row
+  [6,7,8],  # Bottom row
+  [0,3,6],  # Bottom row
+  [1,4,7],  # Bottom row
+  [2,5,8],  # Bottom row
+  [0,4,8],  # Bottom row
+  [2,4,6],  # Bottom row
+]
+
+def won?(board)
+  winner_combination = false
+  WIN_COMBINATIONS.each do |win_combination|
+    win_index_1 = win_combination[0]
+    win_index_2 = win_combination[1]
+    win_index_3 = win_combination[2]
+
+    position_1 = board[win_index_1] # load the value of the board at win_index_1
+    position_2 = board[win_index_2] # load the value of the board at win_index_2
+    position_3 = board[win_index_3] # load the value of the board at win_index_3
+
+    if (position_1 == "X" && position_2 == "X" && position_3 == "X") || (position_1 == "O" && position_2 == "O" && position_3 == "O")
+      winner_combination = win_combination
+    end
+  end
+  return winner_combination
+end
+
+def full?(board)
+  board.none?{|position| position == " "}
+end
+
+def draw?(board)
+  if full?(board) && !won?(board)
+    return true
+  else
+    return false
+  end
+end
+
+def over?(board)
+  if won?(board) || draw?(board) || full?(board)
+    return true
+  else
+    return false
+  end
+end
+
+def winner(board)
+  if won?(board)
+    winning_combination = won?(board)
+    return board[winning_combination[0]]
+  else
+    return nil
+  end
+end
