20f44c962192a5a15611d7c363f715a84d5276e3
Pranav Sharma <ps20maths@gmail.com>
2018-07-04 04:25:12 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,38 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS=  [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [2,4,6]
+]
+def won?(board)
+  WIN_COMBINATIONS.each do |win_combination|
+
+  if board[win_combination[0]]==board[win_combination[1]] && board[win_combination[1]]==board[win_combination[2]] && (board[win_combination[0]]=="X" || board[win_combination[0]]=="O")
+return win_combination
+end
+end
+return false
+end
+def full?(board)
+  board.all? { |i| i=="X" || i=="O"}
+end
+def draw?(board)
+  (!won?(board) && full?(board))
+end
+def over?(board)
+  !(!(won?(board) || full?(board) || draw?(board)))
+end
+def winner(board)
+  win_combination=won?(board)
+  if (win_combination!=false)
+    winning=board[win_combination[0]]
+return winning
+  end
+end
