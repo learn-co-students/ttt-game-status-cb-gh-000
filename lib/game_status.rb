a3c0667f49b0a8bda6dde8e78f694f664e522f41
José Lerma <lalo_pichu22@hotmail.com>
2018-09-08 02:09:18 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -19,11 +19,9 @@ def won?(board)
   WIN_COMBINATIONS.each do |combinations|
     if (board[combinations[0]] == 'X' && board[combinations[1]] == 'X' && board[combinations[2]] == 'X') || (board[combinations[0]] == 'O' && board[combinations[1]] == 'O' && board[combinations[2]] == 'O')
       return combinations
-      break
-    elsif combinations[0] == 2 && combinations[1] == 4 && combinations[2] == 6
-      return false
     end
   end
+  false
 end
 
 def full?(board)
