0f760072c5cae93242445063f8e45a682763d1b5
Matthew Hendrie <unholyagent@gmail.com>
2018-11-30 10:34:33 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,78 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+]
+
+def won?(board)
+  WIN_COMBINATIONS.each do |win_combination|
+    win_index_1 = win_combination[0]
+    win_index_2 = win_combination[1]
+    win_index_3 = win_combination[2]
+
+    position_1 = board[win_index_1]
+    position_2 = board[win_index_2]
+    position_3 = board[win_index_3]
+
+    if position_1 == "X" && position_2 == "X" && position_3 == "X" || position_1 =="O" && position_2 == "O" && position_3 == "O"
+      return win_combination
+    end
+
+  end
+else
+  false
+end
+
+
+def full?(board)
+ if !(board.any?{|i| i == " "})
+   puts "the board is full!"
+   return true
+ else
+   puts "the board is not yet full"
+   return false
+ end
+end
+
+def draw?(board)
+  if full?(board)
+    if won?(board) == false
+      true
+    else
+      false
+    end
+  else
+    false
+  end
+end
+
+def winner(board)
+  winner = nil
+  WIN_COMBINATIONS.each do |combo|
+    if combo.all? {|idx| board[idx] == "X"}
+      winner = "X"
+    elsif combo.all? {|idx| board[idx] == "O"}
+      winner = "O"
+    else
+    end
+  end
+  winner
+end
+
+def over?(board)
+  if full?(board) || won?(board) || draw?(board)
+    puts "GAME OVER"
+    return true
+  else
+    puts "it ain't over till it's over baby!"
+    return false
+  end
+end
