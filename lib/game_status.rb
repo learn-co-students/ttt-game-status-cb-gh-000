9ffb75178ce06f3777d8584912381a429a6930d7
Pavel Torbin <pabin@yandex.ru>
2019-10-28 17:55:53 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,50 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+
+WIN_COMBINATIONS = [
+  [0,1,2], 
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [2,4,6]
+]
+
+def won?(board)
+  WIN_COMBINATIONS.each do |win_combo| # win_combo is same as WIN_COMBINATIONS[0][1][2]
+    if (board[win_combo[0]] == "X" && board[win_combo[1]] == "X" && board[win_combo[2]]== "X") || (board[win_combo[0]] == "O" && board[win_combo[1]] == "O" && board[win_combo[2]] == "O")
+     return win_combo
+    end
+    end
+    return false
+end
+
+
+def full?(arr)
+  arr.all? do |element|
+    element == "X" || element == "O"
+end
+end
+
+
+def draw?(arr)
+  !won?(arr) && full?(arr)
+end
+
+
+def over?(game)
+  won?(game) || draw?(game)
+end
+
+
+def winner(board)
+  # we have game which will be the board array
+  # won?(game) will be either false, or an array (for example: [0,1,2])
+  # we can't grab a value of an index for false
+  if won?(board)
+     board[won?(board)[0]]
+  end
+end
