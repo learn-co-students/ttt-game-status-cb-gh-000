7ed02cfad509a1623d2e97bd9d921bd84ced4b8b
Murat Can Kırdar <muratcankirdar@gmail.com>
2020-01-15 11:06:45 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -1,6 +1,68 @@
-# Helper Method
-def position_taken?(board, index)
-  !(board[index].nil? || board[index] == " ")
+WIN_COMBINATIONS = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,4,8],
+  [2,4,6],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8]
+  ]
+board = ["X", "X", "O", "X", "O", "X", "X", "X", "O"]
+
+def won?(board)
+  winner = nil
+  WIN_COMBINATIONS.each do |combo|
+    if combo.all? {|idx| board[idx] == "X"}
+      winner = combo
+    elsif combo.all? {|idx| board[idx] == "O"}
+      winner = combo
+    else
+      false
+    end
+  end
+  if winner != nil
+    winner
+  else
+    false
+  end
 end
 
-# Define your WIN_COMBINATIONS constant
+def full?(board)
+  board.all? {|token| token == "X" || token == "O"}
+end
+
+def draw?(board)
+  if full?(board)
+    if won?(board) == false
+      true
+    else
+      false
+    end
+  else
+    false
+  end
+end
+
+def winner(board)
+  winner = nil
+  WIN_COMBINATIONS.each do |combo|
+    if combo.all? {|idx| board[idx] == "X"}
+      winner = "X"
+    elsif combo.all? {|idx| board[idx] == "O"}
+      winner = "O"
+    else
+    end
+  end
+  winner
+end
+
+def over?(board)
+  if draw?(board)
+    true
+  elsif won?(board)
+    true
+
+  end
+end
+winner(board)
