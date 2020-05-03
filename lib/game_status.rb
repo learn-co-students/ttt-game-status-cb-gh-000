3243e13b6d9e9f07ba7b6ff1a9f30baf7b7f01dd
Alex Ines <lex@blackcat.watch>
2020-05-03 22:09:25 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,29 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+]
+
+
+def full?
+end
+
+def won?
+end
+
+def draw?
+end
+
+def over?
+end
+
+def winner
+end
